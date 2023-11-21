8b93aeb27911db89930f4a6b506650bb29fd8b8b
pmarino2013 <pmarino2013@gmail.com>
2023-11-21 20:13:04 -0300

    Mejoro diseño y el mensaje

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 
@@ -9,6 +9,7 @@ import "./App.css";
 function App() {
   const MySwal = withReactContent(Swal);
   const [mensaje, setMensaje] = useState("");
+  const opcion = useRef("");
 
   useEffect(() => {
     if (mensaje) {
@@ -24,21 +25,34 @@ function App() {
   };
 
   const alertaMensaje = () => {
-    MySwal.fire(mensaje);
+    MySwal.fire({
+      title: `${mensaje}`,
+      confirmButtonText: "Ok",
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        setMensaje("");
+        opcion.current.value = "";
+      }
+    });
   };
 
   return (
     <div className="container-fluid">
       <div className="container">
-        <div className="row vh-100 justify-content-center align-items-center">
+        <div className="row vh-100 justify-content-center pt-5">
           <div className="col-12 col-md-8 text-center box">
             <img className="logo" src={logo} alt="logo" />
             <h1 className="my-2">Día mundial del saludo 👋</h1>
+
             <form>
               <select
                 className="form-select form-select-lg mb-3"
                 aria-label="Large select example"
                 onChange={mensajeHola}
+                ref={opcion}
               >
                 <option value="">Elige el idioma del saludo🤔</option>
                 {saludos.map((saludo, index) => (
